3e787254568e4e1337cdc18f2eb9bb620f299f23
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-09-17 17:21:37 -0600

    Update ServerJoiner.py

--- ServerJoiner.py
+++ ServerJoiner.py
@@ -1,19 +1,27 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from bs4 import BeautifulSoup
 from selenium_stealth import stealth
-import time
 
-driver = webdriver.Chrome()
+video_id = ""
 
-stealth(driver,
-        languages=["en-US", "en"],
-        vendor="Google Inc.",
-        platform="Win32",
-        webgl_vendor="Intel Inc.",
-        renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True,
-        )
+def mainFunc():
+    driver = webdriver.Chrome()
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
 
-url = "https://www.youtube.com/live_chat?is_popout=1&v=9lhUIEs6HiM"
-driver.get(url)
-time.sleep(5)
-driver.quit()
+    url = f"https://www.youtube.com/live_chat?v={video_id}"
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, features="lxml")
+    mydivs = soup.find_all("div", {"class": "style-scope yt-live-chat-item-list-renderer"})
+    print(mydivs)
+
+mainFunc()
