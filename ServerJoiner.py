2cd384cd2a6804eca61ac0f80f66caa9417c150a
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-09-17 15:44:52 -0600

    Update ServerJoiner.py

--- ServerJoiner.py
+++ ServerJoiner.py
@@ -1,43 +1,19 @@
-import pytchat, re, requests
-channel_url = 'Enter Channel Url Here'
+from selenium import webdriver
+from selenium_stealth import stealth
+import time
 
-def GetLastestStreamId():
-    try:
-        html = requests.get(channel_url+'/streams').text
-        id = int(re.search('(?<="videoId":").*?(?=")',html).group())
-        return id
-    except:
-        return None
+driver = webdriver.Chrome()
 
-def check():
-    request = requests.get(channel_url)
-    TEXT = request.text.encode("ascii", "ignore").decode()
-    if 'hqdefault_live.jpg' in TEXT:
-        return True
-    else: 
-        return False
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
 
-
-def start():
-    Vid_Id = GetLastestStreamId()
-    Is_Live = check()
-    if Is_Live == True:
-        if Vid_Id == None:
-            print('No Video Id')
-        else:
-            chat = pytchat.create(Vid_Id)
-            while chat.is_alive():
-                    for c in chat.get().sync_items():
-                        author= c.author.name.encode("ascii", "ignore").decode()
-                        msg = c.message.encode("ascii", "ignore").decode()
-                        print("Given text contains some URL")
-                        URL = re.search("(?P<url>https?://[^\s]+)", msg).group("url")
-                        if 'https://www.roblox.com/' in URL:
-                            print('Joining Game')
-                        else:
-                            print(f'{author}: {msg}')
-    else:
-        print('Channel Is Not Live')
-        start()
-
-start()
+url = "https://www.youtube.com/live_chat?is_popout=1&v=9lhUIEs6HiM"
+driver.get(url)
+time.sleep(5)
+driver.quit()
