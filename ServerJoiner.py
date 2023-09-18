c8f08de6a4e9a46ef9cd60a5349e5066b79d24b2
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2023-09-18 16:43:21 -0600

    Update ServerJoiner.py

--- ServerJoiner.py
+++ ServerJoiner.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 
+import re
 from bs4 import BeautifulSoup
 from selenium_stealth import stealth
 
 video_id = ""
+
+def getDetails(drivwr, id) -> tuple:
+    drivwr
 
 def mainFunc():
     driver = webdriver.Chrome()
@@ -19,14 +23,15 @@ def mainFunc():
     url = f"https://www.youtube.com/live_chat?v={video_id}"
     driver.get(url)
     latest_id = None
+    
     while True:
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         mydivs = soup.find_all("yt-live-chat-text-message-renderer", {"class": "style-scope yt-live-chat-item-list-renderer"})
         if mydivs[-1]["id"] == latest_id: pass
         else:
-            print(mydivs[-1]["id"])
             latest_id = mydivs[-1]["id"]
-        
+            print(mydivs[-1]["id"])
+            author, msg = getDetails(driver, mydivs[-1]["id"])
         
         
 
