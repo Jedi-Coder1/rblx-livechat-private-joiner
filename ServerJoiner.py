d323736f59d5fc979765a51effa75d6c50b5da24
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-01-11 19:16:42 -0700

    add .gitignore n stuff

--- ServerJoiner.py
+++ ServerJoiner.py
@@ -1,33 +1,6 @@
-import re, requests
-channel_url = 'Enter Channel Url Here'
+from getytchat import ytchat
 
-def GetLastestStreamId():
-    try:
-        html = requests.get(channel_url+'/streams').text
-        id = int(re.search('(?<="videoId":").*?(?=")',html).group())
-        return id
-    except:
-        return None
-
-def check():
-    request = requests.get(channel_url)
-    TEXT = request.text.encode("ascii", "ignore").decode()
-    if 'hqdefault_live.jpg' in TEXT:
-        return True
-    else: 
-        return False
-
-
-def start():
-    Vid_Id = GetLastestStreamId()
-    Is_Live = check()
-    if Is_Live == True:
-        if Vid_Id == None:
-            print('No Video Id')
-        else:
-            pass
-    else:
-        print('Channel Is Not Live')
-        start()
-
-start()
+chat = ytchat("jfKfPfyJRdk")
+while True:
+    author, msg = chat.NextChat()
+    print(f"{author} said: {msg}")
--- getytchat.py
+++ getytchat.py
@@ -8,37 +8,39 @@ from selenium_stealth import stealth
 options = ChromeOptions()
 options.add_argument("-headless")
 
-class ytchat
+class ytchat():
+    def __init__(self, video_id: str) -> None:
+        self.driver = webdriver.Chrome(options=options)
+        self.url = "https://www.youtube.com/live_chat?v=" + video_id
+        stealth(self.driver,
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True,
+                )
+        self.latest_id = None
+        self.driver.get(self.url)
+        # check if video id is valid
+        if "Chat is disabled" in self.driver.page_source: return "Invalid Video ID"
+        else: pass
 
-def getDetails(drivwr, id) -> tuple:
-    container = drivwr.find_element(By.ID, id)
-    # Get Message
-    content = container.find_element(By.ID, "content")
-    message = content.find_element(By.ID, "message")
-    # Get Author
-    authorchip = container.find_element(By.CSS_SELECTOR, "yt-live-chat-author-chip")
-    author = authorchip.find_element(By.ID, "author-name")
-    return (author.text.encode('ascii', 'ignore'), message.text.encode('ascii', 'ignore'))
+    def getDetails(self, id) -> tuple:
+        container = self.driver.find_element(By.ID, id)
+        # Get Message
+        content = container.find_element(By.ID, "content")
+        message = content.find_element(By.ID, "message")
+        # Get Author
+        authorchip = container.find_element(By.CSS_SELECTOR, "yt-live-chat-author-chip")
+        author = authorchip.find_element(By.ID, "author-name")
+        return (author.text.encode('ascii', 'ignore'), message.text.encode('ascii', 'ignore'))
 
-def StartChatRead(video_id):
-    driver = webdriver.Chrome(options=options)
-    stealth(driver,
-            languages=["en-US", "en"],
-            vendor="Google Inc.",
-            platform="Win32",
-            webgl_vendor="Intel Inc.",
-            renderer="Intel Iris OpenGL Engine",
-            fix_hairline=True,
-            )
-
-    url = "https://www.youtube.com/live_chat?v=" + video_id
-    driver.get(url)
-    latest_id = None
-    
-    while True:
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
+    def NextChat(self):
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         mydivs = soup.find_all("yt-live-chat-text-message-renderer", {"class": "style-scope yt-live-chat-item-list-renderer"})
-        if mydivs[-1]["id"] == latest_id: pass
+        if mydivs[-1]["id"] == self.latest_id:
+            return None
         else:
-            latest_id = mydivs[-1]["id"]
-            yield getDetails(driver, mydivs[-1]["id"])
+            self.latest_id = mydivs[-1]["id"]
+            return self.getDetails(mydivs[-1]["id"])
