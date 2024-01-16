c1aa7f5373ee559bb4d96e70764fcd11ede39fd3
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-01-16 16:08:17 -0700

    working

--- ServerJoiner.py
+++ ServerJoiner.py
@@ -1,13 +1,13 @@
-import re
+import re, os
 from getytchat import ytchat
 
-chat = ytchat("")
+chat = ytchat("https://www.youtube.com/watch?v=jfKfPfyJRdk")
 while True:
     nextC = chat.NextChat()
     if nextC:
         author, msg = nextC
         regex = re.search(r"https:\/\/www.roblox\.com\/share.code=[a-z0-9-]+&type=Server", msg.decode())
         if regex:
-            URL = regex.group()
+            os.startfile(regex.group())
         else:
             print(f"{author} said: {msg}")
