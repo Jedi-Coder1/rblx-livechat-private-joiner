9e0328f19ba05c8cb553af832ea22622d0e653ce
Jedi-Coder1 <98861141+Jedi-Coder1@users.noreply.github.com>
2024-01-16 20:33:17 -0700

    launcher

--- ServerJoiner.py
+++ ServerJoiner.py
@@ -1,13 +1,15 @@
-import re, os
+import re, ctypes
 from getytchat import ytchat
 
-chat = ytchat("")
+chat = ytchat("jfKfPfyJRdk")
 while True:
     nextC = chat.NextChat()
     if nextC:
         author, msg = nextC
         regex = re.search(r"https:\/\/www.roblox\.com\/share.code=[a-z0-9-]+&type=Server", msg.decode())
         if regex:
-            os.startfile(regex.group())
+            URL = regex.group()
+            a = ctypes.cdll.LoadLibrary('Launcher.dll')
+            a.launch(URL)
         else:
             print(f"{author} said: {msg}")
